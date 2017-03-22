035fe78d94b05e8a62f77d1524db80caddbf3570
Marat Bakirov <lcdswat@deb-virtual>
2017-03-22 00:33:25 -0500

    indesign upload

---
+++ indesign/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="indesign.css">
+</head>
+<body>
+
+<div class="parallax">
+  <div class="parallax__layer parallax__layer--back">
+    <img src="bg.svg">
+  </div>
+  <div class="parallax__layer parallax__layer--base">
+    <img src="page.png">
+  </div>
+</div>
+
+</body>
+</html>
