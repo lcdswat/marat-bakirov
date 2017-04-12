2e0a5cfd7ca347fac470aaa1a38868b1dc663ecc
Marat Bakirov <lcdswat@deb-virtual>
2017-04-12 05:25:46 -0500

    Добавил типограф и плагин переносов

--- note-test.php
+++ note-test.php
@@ -1,24 +1,25 @@
  <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
 	<title></title>
 	<meta charset="utf-8">
 	<link href="https://fonts.googleapis.com/css?family=Merriweather:300,400,700" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="/site-files/main.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!-- Хипернатор -->
+	<script src="/site-files/hypernator/hyphernator.js"></script>
+	<!-- Типограф -->
+	<script src="/site-files/typograf/dist/typograf.min.js"></script>
+
 </head>
 <body>
-
-	<a name="top"></a>
 
 	<!-- Меню -->
 	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/menu.html'); ?>
 
-	<div class="notes">
-		<p>Всем привет! Сегодня расскажу о&nbsp;том, как сделать нумерацию в&nbsp;индизайне. Эта функция называется &laquo;Объединение данных&raquo; и&nbsp;принцип её&nbsp;работы заключается в&nbsp;том, что вы&nbsp;можете вставлять в&nbsp;индизайновский документ динамические данные из&nbsp;отдельного файла. Для новичков, возможно, звучит мудрено, но&nbsp;по&nbsp;ходу дела всё станет понятно.</p>
-		<p>Чтобы создать файл с&nbsp;данными, нам понадобится эксель. В&nbsp;моем случае лицензии на&nbsp;эксель нет, поэтому я&nbsp;заменяю его бесплатным Опен Офисом.</p>
-		<p>Мы&nbsp;рассмотрим несколько вариантов применения этой функции и&nbsp;начнём, как водится, с&nbsp;простого.</p>
-		<p>Итак, предположим, что у&nbsp;нас есть документ, который представляет из&nbsp;себя входной билет в&nbsp;цирк. В&nbsp;определенном месте на&nbsp;билете должен стоять уникальный порядковый номер. Всего у&nbsp;нас 100&nbsp;билетов. Допустим, номер должен стоять в&nbsp;двух местах&nbsp;&mdash; на&nbsp;отрывном корешке и&nbsp;на&nbsp;основной части.</p>
+	<div class="notes hyphenate">
+		<p>"Мир" - мой мир!!</p>
+		<p>Я - не я</p>
 	</div>
 
 	<!-- Футер -->
@@ -26,5 +27,12 @@
 	
 	<!-- Метрика -->
 	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/metrika.html'); ?>
+
+	<!-- Вызов типографа -->
+	<script>
+ 		var tpRu = new Typograf({locale: 'ru'});
+    	tpRu.enableRule('*');
+	</script>
+
 </body>
 </html>
