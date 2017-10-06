0ea86a30b4e67c6de0dac8e267ea4696ebc5f964
Marat Bakirov <lcdswat@debian-web>
2017-10-06 04:56:13 -0500

    En about

---
+++ en/index-backup.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Marat Bakirov</title>
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/header.html'); ?>
+</head>
+<body>
+
+	<a name="top"></a>
+
+	<!-- Меню -->
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/menu-en.html'); ?>
+
+	<div id="black-bg"></div>
+
+	<!-- Диалог -->
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/hello-text-en.html'); ?>
+
+	<!-- Контент -->
+
+	<!-- Абдульманов -->
+	<p class="white">
+	Сайт для ведущего Азамата Абдульманова<br>
+	Дата релиза: 10.03.2017 г.<br>
+	<a href="https://abdulmanov.com/">www.abdulmanov.com</a>
+	</p>
+	<img src="/i/abdulmanov-1.png" class="grid-i">
+	<img src="/i/abdulmanov-2.png" class="grid-i">
+	<img src="/i/abdulmanov-3.png" class="grid-i">
+	<!-- EOF Абдульманов -->
+
+	<!-- Делосипед -->
+	<p>
+	Дизайн сайта веломастерской «Делосипед»<br>
+	Дата разработки: 05.05.2016 г.<br>
+	</p>
+	<div class="grid">
+		<img src="/i/delosiped.jpg" class="grid-i">
+	</div>
+	<!-- EOF Делосипед -->
+
+	<!-- EOF Контент -->
+
+	<!-- Футер -->
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/footer-en.html'); ?>
+	
+	<!-- Метрика -->
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/metrika.html'); ?>
+</body>
+</html>
--- en/index.php
+++ en/index.php
@@ -1,49 +1,65 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html>
 <head>
-	<title>Marat Bakirov</title>
+	<title>About — Marat Bakirov</title>
 	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/header.html'); ?>
+	<style type="text/css">
+		body {background-color: #201723;}
+	</style>
 </head>
 <body>
 
-	<a name="top"></a>
+	<script type="text/javascript">
+		$(function() {
+		  $('#wait').hide();
+		  setTimeout(function() {
+		    $('#wait').show(400);
+		  }, 500);		  
+		  setTimeout(function() {
+		    $('#wait').hide();
+		    $('#2').show(400);
+		  }, 3000);
+		  setTimeout(function() {
+		    $('#wait').show(400);
+		  }, 6000);		
+		  setTimeout(function() {
+		    $('#wait').hide();
+		    $('#3').show(400);
+		  }, 8000);
+		  setTimeout(function() {
+		    $('#wait').show(400);
+		  }, 11000);
+		  setTimeout(function() {
+		    $('#wait').hide();
+		    $('#one').hide(400);
+		    $('#4').show(400);
+		  }, 13000);
+		  setTimeout(function() {
+		    $('#wait').show(400);
+		  }, 13500);
+		  setTimeout(function() {
+		    $('#wait').hide();
+		    $('#2').hide(400);
+		    $('#5').show(400);
+		  }, 14000);
+		});
+	</script>
+
+	<div id="aboutme">
+		<img src="/site-files/me-pixel-about.gif">
+		<div id="messages">
+			<div class="bubles" id="one"><span></span>Hi! My name is Marat Bakirov</div>
+			<div class="bubles" id="2">I'm graphic designer and layout maker<br>with 10 years of experience</div>
+			<div class="bubles" id="3">I love indesign and nice layouts</div>
+			<div class="bubles" id="4">I also make web sites and know HTML and CSS</div>
+			<div class="bubles" id="5">Contact me with <a href="mailto:lcd-swat@yandex.ru">email</a> or <a href="https://t.me/lcdswat">telegram</a></div>
+			<span id="wait"></span>
+		</div>
+	</div>
 
 	<!-- Меню -->
-	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/menu-en.html'); ?>
+	<?php // include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/menu-en.html'); ?>
 
-	<div id="black-bg"></div>
-
-	<!-- Диалог -->
-	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/hello-text-en.html'); ?>
-
-	<!-- Контент -->
-
-	<!-- Абдульманов -->
-	<p class="white">
-	Сайт для ведущего Азамата Абдульманова<br>
-	Дата релиза: 10.03.2017 г.<br>
-	<a href="https://abdulmanov.com/">www.abdulmanov.com</a>
-	</p>
-	<img src="/i/abdulmanov-1.png" class="grid-i">
-	<img src="/i/abdulmanov-2.png" class="grid-i">
-	<img src="/i/abdulmanov-3.png" class="grid-i">
-	<!-- EOF Абдульманов -->
-
-	<!-- Делосипед -->
-	<p>
-	Дизайн сайта веломастерской «Делосипед»<br>
-	Дата разработки: 05.05.2016 г.<br>
-	</p>
-	<div class="grid">
-		<img src="/i/delosiped.jpg" class="grid-i">
-	</div>
-	<!-- EOF Делосипед -->
-
-	<!-- EOF Контент -->
-
-	<!-- Футер -->
-	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/footer-en.html'); ?>
-	
 	<!-- Метрика -->
 	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/metrika.html'); ?>
 </body>
