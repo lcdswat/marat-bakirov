4e5fa0263aff2061e64d090fbf0bb5d1a41b7a3b
Marat Bakirov <lcdswat@debian-web>
2018-08-08 16:17:38 -0500

    lexus-toyota upload

---
+++ toyota/ok.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="toyota.css">
+
+	<script type="text/javascript" src="jquery.min.js"></script>
+	<script type="text/javascript" src="rangeslider/rangeslider.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="rangeslider/rangeslider.css">
+
+</head> 
+<body>
+
+<a href="/toyota/"><div id="logo"></div></a>
+<h1>Хорошо!</h1>
+
+<p>
+  Наш специалист свяжется с&nbsp;вами в&nbsp;ближайшее время.<br>
+  Хорошего дня!
+</p>
+
+</body>
+</html>
---
+++ toyota/thx.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="toyota.css">
+
+	<script type="text/javascript" src="jquery.min.js"></script>
+	<script type="text/javascript" src="rangeslider/rangeslider.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="rangeslider/rangeslider.css">
+
+</head> 
+<body>
+
+<a href="/toyota/"><div id="logo"></div></a>
+<h1>Спасибо  за&nbsp;ваш отзыв!</h1>
+
+<p>
+Улучшая наши автомобили и&nbsp;повышая уровень сервиса, мы заботимся о&nbsp;том, чтобы максимально соответствовать Вашим ожиданиям.<br>
+Однако мы не хотим предполагать, мы стремимся узнать все из первых рук, у Вас&nbsp;— наших клиентов.<br>
+Вот почему Ваше мнение очень важно для нас.
+</p>
+
+<h1>Хотите, чтобы с&nbsp;вами связался специалист исследовательского центра?</h1>
+<a href="ok.php" class="button">Да</a>
+
+</body>
+</html>
