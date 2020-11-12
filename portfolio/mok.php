a1b24e74e5fc18b574dc559ec31ce87351ab3b47
Marat Bakirov <lcdswat@debian-web>
2020-11-12 07:05:17 -0600

    Ñdobavil portfolio MOK

---
+++ portfolio/mok.php
@@ -0,0 +1,33 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>Портфолио — Московский оружейник и компания</title>
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/header.html'); ?>
+</head>
+<body style="background-color: #d2d3d5;">
+
+	<div id="case-wrapper-margin-10">
+		Moscow gunsmith & Co identy<br>
+		Logo design and visual identy for gunsmith company<br>
+		September 2020<br><br>
+		<img src="/i/MOK-01.svg">
+		<video width="100%" autoplay="true" loop="true" muted="true"><source src="/i/MOK-02.mp4" type="video/mp4"></video>
+		<img src="/i/MOK-03.jpg">
+		<img src="/i/MOK-04.jpg">
+		<img src="/i/MOK-05.jpg">
+		<video width="100%" autoplay="true" loop="true" muted="true"><source src="/i/MOK-06.mp4" type="video/mp4"></video>
+		<img src="/i/MOK-07.jpg">
+		<img src="/i/MOK-08.jpg">
+		<video width="100%" autoplay="true" loop="true" muted="true"><source src="/i/MOK-09.mp4" type="video/mp4"></video>
+		<img src="/i/MOK-10.jpg">
+		<img src="/i/MOK-11.jpg">
+		<video width="100%" autoplay="true" loop="true" muted="true"><source src="/i/MOK-12.mp4" type="video/mp4"></video>
+	</div>
+
+	<!-- Меню -->
+	<?php //include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/menu.html'); ?>
+
+	<!-- Метрика -->
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/metrika.html'); ?>
+</body>
+</html>
