a2169fd7f43983f9dbd15a69b9e708981be2faa0
lcdswat <lcd-swat@yandex.ru>
2021-02-04 14:41:57 +0500

    Поправил страницу МОК

---
+++ portfolio/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Портфолио — Московский оружейник и компания</title>
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/header.html'); ?>
+</head>
+<body>
+
+	<!-- Меню -->
+	<?php //include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/menu.html'); ?>
+
+	<!-- Метрика -->
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/metrika.html'); ?>
+</body>
+</html>
--- portfolio/mok.php
+++ portfolio/mok.php
@@ -1,4 +1,4 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Портфолио — Московский оружейник и компания</title>
@@ -8,12 +8,15 @@
 
 	<div id="case-wrapper-margin-10">
 	
-		<b>Moscow gunsmith & Co identy</b><br><br>
-		Logo design and visual identy for gunsmith company.<br>
-		Made for <a href="\\oranjin.ru">Oranjin</a> agency.<br>
-		September 2020<br><br>
-		<img src="/i/MOK-01.svg">
+		<p>
+			<b>Moscow gunsmith & Co identy</b><br><br>
+			Logo design and visual identy for gunsmith company.<br>
+			Made for <a href="\\oranjin.ru">Oranjin</a> agency.<br>
+			September 2020
+		</p>
+
 		<video width="100%" autoplay loop muted playsinline><source src="/i/MOK-02.mp4" type="video/mp4"></video>
+		<img src="/i/MOK-01.svg">
 		<img src="/i/MOK-03.jpg">
 		<img src="/i/MOK-04.jpg">
 		<img src="/i/MOK-05.jpg">
@@ -24,7 +27,7 @@
 		<img src="/i/MOK-10.jpg">
 		<img src="/i/MOK-11.jpg">
 		<video width="100%" autoplay loop muted playsinline><source src="/i/MOK-12.mp4" type="video/mp4"></video>
-		<br>
+			
 	</div>
 
 	<!-- Меню -->
