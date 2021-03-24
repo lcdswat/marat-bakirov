d6a562248e544ffb22ed942e113e1489e4fddffb
lcdswat <lcd-swat@yandex.ru>
2021-03-24 12:52:37 +0500

    upload

--- pgk/index.php
+++ pgk/index.php
@@ -5,8 +5,6 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="main.css">
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Prosto+One&family=Rubik:ital,wght@0,300;0,400;0,500;0,700;1,300&display=swap" rel="stylesheet">
 </head>
 <body>
 
@@ -15,20 +13,22 @@
 <?php include('menu.html'); ?>
 
 <!-- Обложка -->
-<div id="cover">
+<!-- <div id="cover">
 	<video width="100%" height="100%" muted>
 		<source src="mp4/cover-new.mp4" type="video/mp4">
 	</video>
-</div>
+</div> -->
 
-<img src="2.svg" style="margin: 0; padding: 0; display: block; width: 100%;">
+<?php include('6.svg'); ?>
+<?php include('2.svg'); ?>
+<!-- <img src="2.svg" style="margin: 0; padding: 0; display: block; width: 100%;"> -->
 
 <!-- ПДФ О компании -->
 <?php
-	for($i=2; $i<3; $i++){
-		$s = '<div class="reader"><object id="'. $i .'" data="pdf/'. $i .'.pdf#toolbar=0&navpanes=0&scrollbar=0&view=Fit&viewrect=90,90,90,90" type="application/pdf" width="100%" height="100%" class="pdf"></object></div>';
-	printf("%s\n", $s);
-	}
+	//for($i=2; $i<3; $i++){
+	//	$s = '<div class="reader"><object id="'. $i .'" data="pdf/'. $i .'.pdf#toolbar=0&navpanes=0&scrollbar=0&view=Fit&viewrect=90,90,90,90" type="application/pdf" width="100%" height="100%" class="pdf"></object></div>';
+	//printf("%s\n", $s);
+	//}
 ?>
 
 <!-- <div class="shmutz">
