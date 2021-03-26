814221472a6fa7232c74dd1544579db2ebe8bc6f
lcdswat <lcd-swat@yandex.ru>
2021-03-26 15:14:30 +0500

    upload

--- pgk/index.php
+++ pgk/index.php
@@ -5,6 +5,9 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="main.css">
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+	<!-- Скроллбар -->
+	<link rel="stylesheet" href="https://unpkg.com/simplebar@latest/dist/simplebar.css"/>
+	<script src="https://unpkg.com/simplebar@latest/dist/simplebar.min.js"></script>
 </head>
 <body>
 
@@ -19,19 +22,19 @@
 	</video>
 </div>
 
-<!-- 2-32 -->
-<?php
-	for($i=2; $i<104; $i++){
-		$s = '<object type="image/svg+xml" data="svg/'. $i .'.svg" class="polosa"></object>';
-	printf("%s\n", $s);
-	}
-?>
+<!-- Титул -->
+<object type="image/svg+xml" data="svg/2.svg" class="polosa"></object>
 
-<!-- <div class="shmutz">
-	<video width="100%" height="100%" muted id="shmutz-01">
-		<source src="shmutz.mp4" type="video/mp4">
-	</video>
-</div> -->
+<!-- Обращение -->
+<div class="speech">
+	<div class="text" id="voloshin" data-simplebar data-simplebar-auto-hide="false">
+		<?php include('svg/voloshin.html'); ?>
+	</div>
+	<object type="image/svg+xml" data="svg/speech.svg" class="polosa"></object>
+</div>
+
+<object type="image/svg+xml" data="svg/2.svg" class="polosa"></object>
+
 
 <!-- Прелоадер -->
 <script type="text/javascript">
@@ -51,29 +54,6 @@
 	});
 </script>
 
-<!-- Старт видео на шмуце -->
-<script type="text/javascript">
-	$(document).ready(function(){   
-	    var $element = $('#shmutz-01');
-	    let counter = 0;
-	$(window).scroll(function() {
-	  var scroll = $(window).scrollTop() + $(window).height();
-	  //Если скролл до конца елемента
-	  var offset = $element.offset().top + $element.height();
-	  //Если скролл до начала елемента
-	  //var offset = $element.offset().top
-	 
-	  if (scroll > offset && counter == 0) {
-	    $('#shmutz-01').get(0).play();
-	    counter = 1;
-	  }
-	});
-	$('#shmutz-01').click(function(){
-		$('#shmutz-01').get(0).play();
-	});
-	 
-	   });
-</script>
 
 </body>
 </html>
