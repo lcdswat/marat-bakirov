f9b93254dcd2992cfb67eff35c59ad3e311479f7
lcdswat <lcd-swat@yandex.ru>
2021-05-28 15:25:34 +0500

    final version upload

--- pgk/index.php
+++ pgk/index.php
@@ -34,7 +34,7 @@
 	<div class="gradient" id="bottom"></div>
 	<object type="image/svg+xml" data="content/speech.svg" class="polosa"></object>
 </div>
-<div class="speech" id="4">
+<div class="speech" id="5">
 	<div class="gradient" id="top"></div>
 	<div class="text" id="karataev" data-simplebar data-simplebar-auto-hide="false">
 		<?php include('content/karataev.html'); ?>
@@ -44,10 +44,32 @@
 </div>
 
 <!-- Шмуц 01 -->
-<object type="image/svg+xml" data="content/9.svg" class="polosa" id="shmutz-01"></object>
+<div class="shmutz-wrapper" id="shmutz-01">
+	<div class="shmutz-TOC">
+		<a href="#3">3&nbsp;&nbsp;Обращение Председателя Совета директоров</a><br>
+		<a href="#5">5&nbsp;&nbsp;Обращение Генерального директора</a><br>
+		<a href="#10">10&nbsp;&nbsp;Компания сегодня</a><br>
+		<a href="#11">11&nbsp;&nbsp;География</a><br>
+		<a href="#12">12&nbsp;&nbsp;Вагонный парк</a><br>
+		<a href="#13">13&nbsp;&nbsp;Грузооборот ПГК в 2020 году</a><br>
+		<a href="#14">14&nbsp;&nbsp;Итоги 2020 года</a><br>
+		<a href="#15">15&nbsp;&nbsp;Эффективный ответ на вызовы пандемии COVID-19</a><br>
+		<a href="#17">17&nbsp;&nbsp;Основные события года</a><br>
+		<a href="#20">20&nbsp;&nbsp;Управление устойчивым развитием</a><br>
+		<a href="#23">23&nbsp;&nbsp;Стратегические цели Компании</a><br>
+		<a href="#24">24&nbsp;&nbsp;Обзор рыночного окружения</a><br>
+		<a href="#27">27&nbsp;&nbsp;Обзор операционной деятельности</a><br>
+		<a href="#33">33&nbsp;&nbsp;Проекты цифровизации ПГК</a><br>
+		<a href="#36">36&nbsp;&nbsp;Анализ финансовых результатов и финансового положения</a><br>
+		<a href="#37">37&nbsp;&nbsp;Ключевые результаты</a><br>
+		<a href="#45">45&nbsp;&nbsp;Финансовая отчетность по МСФО</a><br>
+		<a href="#50">50&nbsp;&nbsp;Основные показатели за 3 года</a>
+	</div>
+	<object type="image/svg+xml" data="content/9.svg" class="polosa"></object>
+</div>
 
 <!-- 10 полоса -->
-<object type="image/svg+xml" data="content/10.svg" class="polosa" id="6"></object>
+<object type="image/svg+xml" data="content/10.svg" class="polosa" id="10"></object>
 
 <!-- География -->
 <?php include('content/map.html'); ?>
@@ -75,42 +97,73 @@
 </div>
 <object type="image/svg+xml" data="content/35.svg" class="polosa" id="35"></object>
 
-<!-- 36—49 -->
+<!-- 36—50 -->
 <?php
-	for($i=36; $i<=49; $i++){
+	for($i=36; $i<=50; $i++){
 		$s = '<object type="image/svg+xml" data="content/'. $i .'.svg" class="polosa" id="'. $i .'"></object>';
 	printf("%s\n", $s);
 	}
 ?>
 
 <!-- Шмуц 02 -->
-<object type="image/svg+xml" data="content/50.svg" class="polosa" id="shmutz-02"></object>
+<div class="shmutz-wrapper" id="shmutz-02">
+	<div class="shmutz-TOC">
+		<a href="#52">52&nbsp;&nbsp;Система корпоративного управления</a><br>
+		<a href="#52">52&nbsp;&nbsp;Принципы корпоративного управления</a><br>
+		<a href="#54">54&nbsp;&nbsp;Структура корпоративного управления</a><br>
+		<a href="#56">56&nbsp;&nbsp;Уставный капитал и акционерная структура</a><br>
+		<a href="#57">57&nbsp;&nbsp;Совет директоров</a><br>
+		<a href="#70">70&nbsp;&nbsp;Исполнительные органы управления</a><br>
+		<a href="#77">77&nbsp;&nbsp;Органы контроля</a><br>
+		<a href="#79">79&nbsp;&nbsp;Управление рисками</a><br>
+		<a href="#82">82&nbsp;&nbsp;Информационная политика</a><br>
+		<a href="#83">83&nbsp;&nbsp;Противодействие коррупции</a>
+	</div>
+	<object type="image/svg+xml" data="content/51.svg" class="polosa"></object>
+</div>
 
-<!-- 51—83 -->
+<!-- 52—83 -->
 <?php
-	for($i=51; $i<=83; $i++){
+	for($i=52; $i<=83; $i++){
 		$s = '<object type="image/svg+xml" data="content/'. $i .'.svg" class="polosa" id="'. $i .'"></object>';
 	printf("%s\n", $s);
 	}
 ?>
 
 <!-- Шмуц 03 -->
-<object type="image/svg+xml" data="content/84.svg" class="polosa" id="shmutz-03"></object>
+<div class="shmutz-wrapper" id="shmutz-03">
+	<div class="shmutz-TOC">
+		<a href="#85">85&nbsp;&nbsp;Взаимодействие с заинтересованными сторонами</a><br>
+		<a href="#88">88&nbsp;&nbsp;Принципы ПГК при управлении персоналом</a><br>
+		<a href="#91">91&nbsp;&nbsp;Социальная ответственность</a><br>
+		<a href="#93">93&nbsp;&nbsp;Охрана труда и промышленная безопасность</a><br>
+		<a href="#95">95&nbsp;&nbsp;Экология и рациональное использование ресурсов</a>
+	</div>
+	<object type="image/svg+xml" data="content/84.svg" class="polosa"></object>
+</div>
 
 <!-- 85—97 -->
 <?php
-	for($i=85; $i<=97; $i++){
+	for($i=85; $i<=96; $i++){
 		$s = '<object type="image/svg+xml" data="content/'. $i .'.svg" class="polosa" id="'. $i .'"></object>';
 	printf("%s\n", $s);
 	}
 ?>
 
 <!-- Шмуц 04 -->
-<object type="image/svg+xml" data="content/98.svg" class="polosa" id="shmutz-04"></object>
+<div class="shmutz-wrapper" id="shmutz-04">
+	<div class="shmutz-TOC">
+		<a href="#98">98&nbsp;&nbsp;Cведения о сделках, в совершении которых имеется заинтересованность. Крупные сделки</a><br>
+		<a href="#99">99&nbsp;&nbsp;Заключение Управления внутреннего аудита об управлении рисками и внутреннем контроле Общества за&nbsp;2020 год</a><br>
+		<a href="#100">100&nbsp;&nbsp;Глоссарий</a><br>
+		<a href="#102">102&nbsp;&nbsp;Контактная информация для акционеров и инвесторов</a>
+	</div>
+	<object type="image/svg+xml" data="content/97.svg" class="polosa"></object>
+</div>
 
-<!-- 99-103 -->
+<!-- 98-103 -->
 <?php
-	for($i=99; $i<=103; $i++){
+	for($i=98; $i<=103; $i++){
 		$s = '<object type="image/svg+xml" data="content/'. $i .'.svg" class="polosa" id="'. $i .'"></object>';
 	printf("%s\n", $s);
 	}
@@ -132,30 +185,6 @@
 	    	$('#cover video').get(0).play();
 	    },1000);
 	});
-</script>
-
-<!-- Анимация на шмуце 01 -->
-<script type="text/javascript">
-	$(document).ready(function(){   
-	    var $element = $('#shmutz-01');
-	    let counter = 0;
-	$(window).scroll(function() {
-	  var scroll = $(window).scrollTop() + $(window).height();
-	  //Если скролл до конца елемента
-	  var offset = $element.offset().top + $element.height();
-	  //Если скролл до начала елемента
-	  //var offset = $element.offset().top
-	 
-	  if (scroll > offset && counter == 0) {
-	    $('.7df4bcb0-90da-44d5-95ea-5c25244e9685').css('display: none;');
-	    counter = 1;
-	  }
-	});
-	$('#shmutz-01').click(function(){
-		$('#shmutz-01').get(0).play();
-	});
-	 
-	   });
 </script>
 
 
