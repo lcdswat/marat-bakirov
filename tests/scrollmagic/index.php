17def5853c05ba4c2a743963ee732ce9105a774d
lcdswat <lcd-swat@yandex.ru>
2021-11-29 19:35:45 +0500

    Update index.php

--- tests/scrollmagic/index.php
+++ tests/scrollmagic/index.php
@@ -89,7 +89,7 @@
 	var controller = new ScrollMagic.Controller();
 
 	// build scene
-	var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 4000, triggerHook: 0})
+	var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 1000, triggerHook: 0})
 					.setPin('#imagesequence')
 					.setTween(tween)
 					.addIndicators() // add indicators (requires plugin)
