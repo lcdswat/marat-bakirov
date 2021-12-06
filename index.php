1b77d78a10d87c3b6bafe72443c905d056f5c6ab
lcdswat <lcd-swat@yandex.ru>
2021-12-06 13:22:53 +0500

    Аплоад
    Добавил ссылку на портфолио, переделал появление баблов, поменял структуру файлов

--- index.php
+++ index.php
@@ -17,42 +17,42 @@
 		  }, 500);		  
 		  setTimeout(function() {
 		    $('#wait').hide();
-		    $('#2').show(400);
+		    $('#bub-2').fadeIn(400);
 		  }, 3000);
 		  setTimeout(function() {
 		    $('#wait').show(400);
 		  }, 6000);		
 		  setTimeout(function() {
 		    $('#wait').hide();
-		    $('#3').show(400);
+		    $('#bub-3').fadeIn(400);
 		  }, 8000);
 		  setTimeout(function() {
 		    $('#wait').show(400);
-		  }, 11000);
+		  }, 9000);
 		  setTimeout(function() {
 		    $('#wait').hide();
-		    $('#one').hide(400);
-		    $('#4').show(400);
-		  }, 13000);
+		    $('#bub-1').hide(400);
+		    $('#bub-4').fadeIn(400);
+		  }, 9500);
 		  setTimeout(function() {
 		    $('#wait').show(400);
-		  }, 13500);
+		  }, 15500);
 		  setTimeout(function() {
 		    $('#wait').hide();
-		    $('#2').hide(400);
-		    $('#5').show(400);
-		  }, 14000);
+		    $('#bub-2').hide(400);
+		    $('#bub-5').fadeIn(400);
+		  }, 17000);
 		});
 	</script>
 
 	<div id="aboutme">
 		<img src="/site-files/me-pixel-about.gif">
 		<div id="messages">
-			<div class="bubles" id="one"><span></span>Привет! Меня зовут Марат Бакиров</div>
-			<div class="bubles" id="2">Я занимаюсь графическим дизайном<br>и вёрсткой более 10 лет</div>
-			<div class="bubles" id="3">Люблю индизайн и хорошую вёрстку</div>
-			<div class="bubles" id="4">Ещё я делаю сайты и всякое такое</div>
-			<div class="bubles" id="5">Если что&nbsp;&mdash; пишите на почту <a href="mailto:lcd-swat@yandex.ru">lcd-swat@yandex.ru</a><br>или в <a href="https://t.me/lcdswat">телеграм</a></div>
+			<div class="bubles" id="bub-1"><span></span>Привет! Меня зовут Марат Бакиров</div>
+			<div class="bubles" id="bub-2">Я занимаюсь графическим дизайном и&nbsp;вёрсткой с&nbsp;2007 года. Обалдеть, конечно. Время летит!</div>
+			<div class="bubles" id="bub-3">Посмотрите портфолио на <a href="https://www.behance.net/marat-bakirov">бихансе</a></div>
+			<div class="bubles" id="bub-4">Если что&nbsp;&mdash; пишите на&nbsp;почту <a href="mailto:lcd-swat@yandex.ru"><nobr>lcd-swat@yandex.ru</nobr></a><br>или в <a href="https://t.me/lcdswat"><nobr>телеграм</nobr></a></div>
+			<div class="bubles" id="bub-5">А я пока тут постою. В майке и шортах зимой и летом</div>
 			<span id="wait"></span>
 		</div>
 	</div>
